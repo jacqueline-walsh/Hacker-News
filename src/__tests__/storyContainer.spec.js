6d6819f1b1da738b40273e61bb6e33de28a38038
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2022-09-18 11:27:56 +0100

    add unit test and update story link

---
+++ src/__tests__/storyContainer.spec.js
@@ -0,0 +1,28 @@
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { individualStory } from "../mocks";
+import axios from "axios";
+import { getStory } from "../services/api";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock("axios");
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe("Render Story test suite", () => {
+  it("renders component with content", async () => {
+    axios.get.mockImplementationOnce(() =>
+      Promise.resolve({ data: individualStory })
+    );
+    const entity = await getStory(1);
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(
+      "https:/hacker-news.firebaseio.com/v0/item/1.json"
+    );
+    expect(entity.title).toEqual("Zoom Goes Down");
+    expect(entity.by).toEqual("Joe Blogs");
+  });
+});
