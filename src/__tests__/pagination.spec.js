88a88952622e3bb73778edc097fcceeea39e7b90
rodu <3225111+rodu@users.noreply.github.com>
2022-09-21 11:14:57 +0100

    Adds test to check the rendering of a single page

--- src/__tests__/pagination.spec.js
+++ src/__tests__/pagination.spec.js
@@ -4,9 +4,19 @@ import { shallow, mount, render } from "enzyme";
 import Pagination from "../components/pagination/Pagination";
 
 describe("Pagination", () => {
-  it("should render the pagination div", () => {
-    const component = mount(<Pagination />);
+  describe("when instantiating the component", () => {
+    it("should render the pagination div", () => {
+      const component = mount(<Pagination />);
 
-    expect(component.find(".pagination").length).toBe(1);
+      expect(component.find(".pagination").length).toBe(1);
+    });
+  });
+
+  describe("when there is a single story", () => {
+    it("should render 1 page button", () => {
+      const component = mount(<Pagination totalStories={1} storiesPerPage={10} />);
+
+      expect(component.find("button").length).toBe(1);
+    });
   });
 });
